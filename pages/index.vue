86a52516d8ea726c92cbc752bf65114b21eab5d8
Tara Z. Manicsic <tzmanics@gmail.com>
2023-03-07 17:47:32 -0500

    removes app.vue and landing page work

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Trying to Manage</h1>
+  <main>
+    <header>
+      <h1 class="logo">Trying to Manage</h1>
+      <h3><a href="https://www.youtube.com/watch?v=vjW8wmF5VWc">
+        Watch me work, watch me try to be a good manager.
+      </a></h3>
+      <nav>
+        <NuxtLink to="/vision-statement">
+          Vision Statement
+        </NuxtLink>
+      </nav>
+    </header>
+  </main>
 </template>
---
+++ pages/vision-statement.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="vision-page">
+    <h1>Trying to Manage: A Vision Statement</h1>
+    <hr>
+    <p>I don't know if what I'm doing is right, or good, or if it makes sense outside of my own brain. Like many things in my life, there is a fantastic probability that I'll look back and say, "Oh, you silly goose, there was a much better way to handle that."</p>
+    
+    <p>But, one thing I can do is use this platform to act as a log to look back on. I can write these posts to remind myself where n00bie Tara came up with these ideas, possibly help others and future Tara to avoid making similar mistakes. Oooor just exist as the origin story of the evil manager that, after years of the cruel, cruel business world, is merely a husk of the bright-eyed, humane, smiling woman she once was. You know, one of those options.</p>
+
+    <p>Either way, join me on this journey of figuring out how to try to manage.</p>
+  </div>
+</template>
